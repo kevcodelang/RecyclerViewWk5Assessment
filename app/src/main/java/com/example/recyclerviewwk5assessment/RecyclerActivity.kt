10caf1b6d3dad674407927176daa9bcdb1621bad
Kdoni <63546827+Kdoni@users.noreply.github.com>
2021-07-05 16:26:22 +0100

    Initial commit

---
+++ app/src/main/java/com/example/recyclerviewwk5assessment/RecyclerActivity.kt
@@ -0,0 +1,124 @@
+package com.example.recyclerviewwk5assessment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.recyclerviewwk5assessment.adapter.ProfileAdapter
+import com.example.recyclerviewwk5assessment.databinding.ActivityMainBinding
+import com.example.recyclerviewwk5assessment.databinding.ActivityRecyclerBinding
+import com.example.recyclerviewwk5assessment.models.Profile
+
+class RecyclerActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRecyclerBinding
+    private lateinit var myProfileAdapter: ProfileAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRecyclerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val allProfile: List<Profile> = listOf(
+            Profile(
+                R.drawable.ic_1,
+                "Kelvin",
+                "kotlin",
+            ),
+            Profile(
+                R.drawable.ic_5,
+                "Liam",
+                "Noah",
+            ),
+            Profile(
+                R.drawable.ic_6,
+                "Oliver",
+                "Elijah",
+            ),
+            Profile(
+                R.drawable.ic_1,
+                "Emma",
+                "Joseph",
+            ),
+            Profile(
+                R.drawable.ic_5,
+                "John",
+                "Olivia",
+            ),
+            Profile(
+                R.drawable.ic_6,
+                "Peace",
+                "Olivia",
+            ),
+            Profile(
+                R.drawable.ic_1,
+                "Eva",
+                "William",
+            ),
+            Profile(
+                R.drawable.ic_5,
+                "Bright",
+                "James",
+            ),
+            Profile(
+                R.drawable.ic_6,
+                "Sophia",
+                "Henry",
+            ),
+            Profile(
+                R.drawable.ic_1,
+                "Mia",
+                "Alexander",
+            ),
+            Profile(
+                R.drawable.ic_5,
+                "Evelyn",
+                "Harper",
+            ),
+            Profile(
+                R.drawable.ic_6,
+                "Desmond",
+                "Brown",
+            ),
+            Profile(
+                R.drawable.ic_1,
+                "Peace",
+                "Olivia",
+            ),
+            Profile(
+                R.drawable.ic_5,
+                "Eva",
+                "William",
+            ),
+            Profile(
+                R.drawable.ic_6,
+                "Bright",
+                "James",
+            ),
+            Profile(
+                R.drawable.ic_1,
+                "Sophia",
+                "Henry",
+            ),
+            Profile(
+                R.drawable.ic_5,
+                "Mia",
+                "Alexander",
+            ),
+            Profile(
+                R.drawable.ic_6,
+                "Evelyn",
+                "Harper",
+            ),
+            Profile(
+                R.drawable.ic_1,
+                "Desmond",
+                "Brown",
+            ),
+            Profile(
+                R.drawable.ic_5,
+                "Desmond",
+                "Brown",
+            )
+        )
+        myProfileAdapter = ProfileAdapter(allProfile)
+        binding.profileRecyclerView.adapter = myProfileAdapter
+    }
+}
---
+++ app/src/main/java/com/example/recyclerviewwk5assessment/adapter/ProfileAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.recyclerviewwk5assessment.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewwk5assessment.databinding.ProfileItemBinding
+import com.example.recyclerviewwk5assessment.models.Profile
+
+class ProfileAdapter(val allProfile: List<Profile>) :
+    RecyclerView.Adapter<ProfileAdapter.ProfileViewHolder>() {
+    class ProfileViewHolder(var binding: ProfileItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(profile: Profile) {
+            binding.profileImages.setImageResource(profile.profileImage)
+            binding.firstName.text = profile.firstName
+            binding.lastName.text = profile.lastName
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfileViewHolder {
+        var binding = ProfileItemBinding.inflate(LayoutInflater.from(parent.context))
+        return ProfileViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ProfileViewHolder, position: Int) {
+        val profile = allProfile[position]
+        holder.bind(profile)
+    }
+
+    override fun getItemCount(): Int {
+        return allProfile.size
+    }
+
+}
